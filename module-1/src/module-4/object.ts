a6f710f301a99c12e04f4046a68b7dd655c316a1
rafiqulshopon <rafiqul.dev@gmail.com>
2023-05-06 23:54:30 +0600

    learned inheritence

--- module-1/src/module-4/object.ts
+++ module-1/src/module-4/object.ts
@@ -15,17 +15,17 @@
 
 // parameter properties
 
-class Animal {
-  constructor(
-    public name: string,
-    public species: string,
-    public sound: string
-  ) {}
-  makeSound() {
-    console.log(`This ${this.name} says ${this.sound}`);
-  }
-}
+// class Animal {
+//   constructor(
+//     public name: string,
+//     public species: string,
+//     public sound: string
+//   ) {}
+//   makeSound() {
+//     console.log(`This ${this.name} says ${this.sound}`);
+//   }
+// }
 
-const dog = new Animal('German Shepard', 'dog', 'gheu gheu');
+// const dog = new Animal('German Shepard', 'dog', 'gheu gheu');
 
-dog.makeSound();
+// dog.makeSound();
