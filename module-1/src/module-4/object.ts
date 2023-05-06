4b89d844f938462c0f39de9ef09f33ca76bef9f9
rafiqulshopon <rafiqul.dev@gmail.com>
2023-05-06 22:49:47 +0600

    learned overview of class

---
+++ module-1/src/module-4/object.ts
@@ -0,0 +1,31 @@
+// class Animal {
+//   name: string;
+//   species: string;
+//   sound: string;
+
+//   constructor(name: string, species: string, sound: string) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
+//   makeSound() {
+//     console.log(`This ${this.name} says ${this.sound}`);
+//   }
+// }
+
+// parameter properties
+
+class Animal {
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string
+  ) {}
+  makeSound() {
+    console.log(`This ${this.name} says ${this.sound}`);
+  }
+}
+
+const dog = new Animal('German Shepard', 'dog', 'gheu gheu');
+
+dog.makeSound();
